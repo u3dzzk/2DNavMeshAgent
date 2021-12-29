caa37f72136f4d50530f241de8f0740e6872dbd3
zzk <786116604@qq.com>
2021-12-29 18:38:02 +0800

    2D寻路

---
+++ 2DNAV/Assets/MyCilet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PolyNav;
+
+public class MyCilet : MonoBehaviour
+{
+    public Camera main;
+    public PolyNavAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            agent.SetDestination(main.ScreenToWorldPoint(Input.mousePosition));
+            
+        }
+    }
+}
